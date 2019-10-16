e4ac1d4f0d2a3f6075b0d1c0f3da4ce0589c43ad
ryuki.abe <r.a.nowitzki41@softbank.ne.jp>
2019-10-16 09:44:43 +0000

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,9 +38,16 @@ class UsersController < ApplicationController
     @followers = @user.followers.page(params[:page])
     counts(@user)
   end
-
+  
+  def likes
+    @user = User.find(params[:id])
+    @micropost = Micropost.order(id: :desc).page(params[:page]).per(25)
+    @favorites = @user.favposts.page(params[:page])
+    counts(@user) 
+  end
+  
   private
-
+  
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
